9a188e0d18b9f20cdee4beb62491cd7fca3dbd65
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-08-25 15:48:28 -0700

    Posts:: useNavigate instead of Link to go back to Home page

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import axios from 'axios';
 
 export default function Posts() {
+  let navigate = useNavigate();
   const { id } = useParams();
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState();
@@ -32,7 +33,7 @@ export default function Posts() {
   return (
     <>
       <div className="post__search">
-        <button>← Back</button>
+          <button onClick={() => navigate('/')}>← Back</button>
         <div className="post__search--container">
           <label className="post__search--label">Search by Id</label>
           <input

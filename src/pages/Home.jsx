ffb66b9fa59a3ccfcb920b5cd79707ab151ff6aa
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-08-25 16:27:01 -0700

    Home:: Setting up and getting users data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 export default function Home() {
+    const [users, setUsers] = useState([]);
+
+    async function getUsers() {
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+        setUsers(data);
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+        
   return (
     <div className="container">
         <div className="row">
-            HOME
+            <div className="user-list">
+            {users.map((user) => (
+                <div className="user" key={user.id}>
+                    <div className="user-card">
+                    <div className="user-card__container">
+
+                            <h3>{user.name}</h3>
+                        <p>
+                        <b>Email:</b> {user.email}
+                        </p>
+                        <p>
+                        <b>Phone:</b> {user.phone}
+                        </p>
+                        <p>
+                        <b>Website:</b>
+                        {user.website}
+                        </p>
+                    </div>
+                    </div>
+                </div>
+            ))}
+            </div>
         </div>
     </div>
   )
